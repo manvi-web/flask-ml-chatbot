f90fd6b3fd4ba579a968efa7567d0a681d24b19f
yashodhakumari <yashodha.kumari@spoors.in>
2025-04-14 12:04:59 +0530

    added app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -6,16 +6,23 @@ app = Flask(__name__)
 df = pd.read_csv("faq_dataset.csv")
 vectorizer = TfidfVectorizer()
 tfidf_matrix = vectorizer.fit_transform(df['question'])
+@app.route('/')
+def index():
+    return render_template('index.html') 
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.get_json()
     question = data.get("question", "")
+    
+    if not question:
+        return jsonify({"answer": "Please provide a valid question."})
+    
     input_vec = vectorizer.transform([question])
     similarity = cosine_similarity(input_vec, tfidf_matrix)
     idx = similarity.argmax()
     answer = df.iloc[idx]["answer"]
+    
     return jsonify({"answer": answer})
 
 if __name__ == '__main__':
-   
     app.run(port=10000, debug=True)
